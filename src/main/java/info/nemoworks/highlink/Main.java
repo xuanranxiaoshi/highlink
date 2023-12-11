e17e1043e77f1a2e01f51da00caec63c137d0612
Chun <caochun@gmail.com>
2023-12-11 11:25:08 +0800

    add comments

--- src/main/java/info/nemoworks/highlink/Main.java
+++ src/main/java/info/nemoworks/highlink/Main.java
@@ -32,29 +32,30 @@ public class Main {
                 JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(),
                 true);
 
+        //读取json文件，模拟数据接收系统收到上传数据
         JsonNode enWasteRec = mapper.readTree(Main.class.getResourceAsStream("/TBL_ENWASTEREC.json"));
         JsonNode exWasteRec = mapper.readTree(Main.class.getResourceAsStream("/TBL_EXWASTEREC.json"));
         JsonNode gantryWasteRec = mapper.readTree(Main.class.getResourceAsStream("/TBL_GANTRYWASTEREC.json"));
         JsonNode parkWasteRec = mapper.readTree(Main.class.getResourceAsStream("/TBL_PARKTRANSWASTEREC.json"));
 
+        //用json中的对象生成数据流（用while true循环模拟无限数据）
         DataStream<HighwayTransaction> enWaste = env
                 .addSource(new TransactionSource(enWasteRec, "entry"))
                 .name("ENTRY_WASTE");
-
         DataStream<HighwayTransaction> exWaste = env
                 .addSource(new TransactionSource(exWasteRec, "exit"))
                 .name("EXIT_WASTE");
-
         DataStream<HighwayTransaction> gantryWaste = env
                 .addSource(new TransactionSource(gantryWasteRec, "gantry"))
                 .name("GANTRY_WASTE");
-
         DataStream<HighwayTransaction> parkWaste = env
                 .addSource(new TransactionSource(parkWasteRec, "park"))
                 .name("PARK_WASTE");
 
+        //将四个独立的流合并为一个，模拟接收系统向预处理系统传递数据
         DataStream<HighwayTransaction> unionStream = enWaste.union(exWaste).union(gantryWaste).union(parkWaste);
 
+        //将数据流按规则进行拆分
         final OutputTag<ExitTransaction> exitTrans = new OutputTag<ExitTransaction>("exitTrans") {
         };
         final OutputTag<ParkTransaction> parkTrans = new OutputTag<ParkTransaction>("parkTrans") {
@@ -90,10 +91,11 @@ public class Main {
         DataStream<GantryTransaction> gantryStream = mainDataStream.getSideOutput(gantryTrans);
         DataStream<ExitTransaction> exitStream = mainDataStream.getSideOutput(exitTrans);
         DataStream<ParkTransaction> parkStream = mainDataStream.getSideOutput(parkTrans);
+        DataStream<EntryTransaction> entryStream = mainDataStream;
 
-        mainDataStream.map(new LinkCounter<EntryTransaction>("main"))
+        //得到四个不同类型的数据流
+        entryStream.map(new LinkCounter<EntryTransaction>("entry"))
                 .addSink(new TransactionSink<EntryTransaction>(TransactionSink.ANSI_YELLOW));
-
         gantryStream.map(new LinkCounter<GantryTransaction>("gantry"))
                 .addSink(new TransactionSink<GantryTransaction>(TransactionSink.ANSI_BLUE));
         exitStream.map(new LinkCounter<ExitTransaction>("exit"))
@@ -102,6 +104,7 @@ public class Main {
                 .addSink(new TransactionSink<ParkTransaction>(TransactionSink.ANSI_GREEN));
 
 
+        //配置flink集群，启动任务
         MiniClusterConfiguration clusterConfiguration = new MiniClusterConfiguration.Builder()
                 .setNumTaskManagers(2)
                 .setNumSlotsPerTaskManager(4).build();
