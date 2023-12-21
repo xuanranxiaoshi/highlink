8bd39c0b8525f91b1cb19c302793ad929fc694c7
csc <2479966011@qq.com>
2023-12-21 14:11:20 +0800

    sql test

--- src/test/java/info/nemoworks/highlink/model/mapper/CreateTableSQL.java
+++ src/test/java/info/nemoworks/highlink/model/mapper/CreateTableSQL.java
@@ -3,13 +3,7 @@ package info.nemoworks.highlink.model.mapper;
 import info.nemoworks.highlink.connector.JdbcConnectorHelper;
 import info.nemoworks.highlink.model.TollChangeTransactions;
 
-/**
- * @description:
- * @author：jimi
- * @date: 2023/12/20
- * @Copyright：
- */
-public class SQL {
+public class CreateTableSQL {
     public static void main(String[] args) {
         String createTableString = JdbcConnectorHelper.getCreateTableString(TollChangeTransactions.class);
         System.out.println(createTableString);
