40177d89b1e19430546e56f7164f287c9ac56665
xiaoshi <2479966011@qq.com>
2024-05-13 14:33:40 +0800

    feat: 清分业务测试完成
    没有集成自动建表，需手动在 ClickHouse 中建表

--- src/test/java/info/nemoworks/highlink/clickHouse/TestBasicOp.java
+++ src/test/java/info/nemoworks/highlink/clickHouse/TestBasicOp.java
@@ -1,12 +1,20 @@
 package info.nemoworks.highlink.clickHouse;
 
+import info.nemoworks.highlink.connector.JdbcConnectorHelper;
+import info.nemoworks.highlink.utils.Config;
+import lombok.Data;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.lang.reflect.Field;
+import java.sql.*;
 
 /**
  * @description:
@@ -16,12 +24,16 @@ import java.sql.Statement;
  */
 public class TestBasicOp {
 
+    // JDBC 驱动和数据库 URL
+    String JDBC_DRIVER = "com.clickhouse.jdbc.ClickHouseDriver";
+    String DB_URL = "jdbc:clickhouse://localhost:18123/highLinks";
+
+    String user = "default";
+
+    String password = "";
+
     @Test
     public void TestConnect() throws SQLException {
-
-        // JDBC 驱动和数据库 URL
-        String JDBC_DRIVER = "com.clickhouse.jdbc.ClickHouseDriver";
-        String DB_URL = "jdbc:clickhouse://localhost:18123/highLinks";
 
         Connection conn = null;
         Statement stmt = null;
@@ -31,7 +43,7 @@ public class TestBasicOp {
 
             // 打开连接
             System.out.println("连接到 ClickHouse 数据库...");
-            conn = DriverManager.getConnection(DB_URL);
+            conn = DriverManager.getConnection(DB_URL, "default", "");
 
             // 创建 Statement 对象
             stmt = conn.createStatement();
@@ -62,4 +74,80 @@ public class TestBasicOp {
 
         System.out.println("查询完毕!");
     }
+
+
+//    @Test
+//    public void TestSink() throws Exception {
+//        // 创建执行环境
+//        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        // 创建输入数据源
+//        String[] words = new String[] {"csc1", "csc2", "csc3", "csc4"};
+//        // 将数组转换为Flink数据流
+//        DataStream<String> dataSource = env.fromElements(words);
+//
+//        SingleOutputStreamOperator<Student> map = dataSource.map(new MapFunction<String, Student>() {
+//            static int num = 0;
+//            @Override
+//            public Student map(String value) throws Exception {
+//                Student student = new Student(value, ++num);
+//                System.out.println(student);
+//                return student;
+//            }
+//        });
+//
+//
+//        JdbcConnectionOptions build = new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+//                .withUrl(Config.getProperty("CH.url"))
+//                .withDriverName(Config.getProperty("CH.driver-class-name"))
+//                .withUsername(Config.getProperty("CH.username"))
+//                .withPassword(Config.getProperty("CH.password"))
+//                .build();
+//
+//
+//
+//        // 创建 ClickHouse 连接配置
+//        DataStreamSink<Student> sink = map.addSink(JdbcSink.sink(
+//                "INSERT INTO STUDENT ( NUM, ID, NAME, AGE, GRADE) VALUES( ?, ?, ?, ?, ?)",
+//                (statement, Student) -> {
+//                    statement.setInt(1, Student.num);
+//                    statement.setInt(2, Student.id);
+//                    statement.setString(3, Student.name);
+//                    statement.setInt(4, Student.age);
+//                    statement.setString(5, Student.grade);
+//                },
+//                build
+//        ));
+//
+//        // 启动流处理程序
+//        env.execute("Flink Array Source Demo");
+//    }
+//
+//
+//    @Test
+//    public void testStudent(){
+//        JdbcConnectorHelper.getCreateTableString(Student .class);
+//        System.out.println(JdbcConnectorHelper.getInsertTemplateString(Student.class));
+//    }
+
 }
+
+@Data
+class Student{
+    int num;
+    int id;
+    String name;
+
+    int age;
+
+    String grade;
+
+    public Student(String name, int id){
+        this.name = name;
+        this.id = id;
+        this.num = id;
+        this.age=22;
+        this.grade ="A";
+    }
+}
+
+
